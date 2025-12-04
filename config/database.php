1b5b7637660959f327d00181c2cf40644fa5391b
wlvUmar <U.Tolibov@wlv.ac.uk>
2025-12-04 16:53:52 +0500

    pasword change

--- config/database.php
+++ config/database.php
@@ -2,5 +2,5 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'kuplyuta_db');
 define('DB_USER', 'kuplyuta_admin');
-define('DB_PASS', '#Getout04');
+define('DB_PASS', '=@iX?z~gukWg');
 define('DB_CHARSET', 'utf8mb4');
