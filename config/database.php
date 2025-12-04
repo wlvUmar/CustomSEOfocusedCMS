2749e6ebd7775bf8d4087ede5c402ca1af9da495
wlvUmar <U.Tolibov@wlv.ac.uk>
2025-12-04 16:52:13 +0500

    config changes

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'appliances_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_NAME', 'kuplyuta_db');
+define('DB_USER', 'kuplyuta_admin');
+define('DB_PASS', '#Getout04');
 define('DB_CHARSET', 'utf8mb4');
