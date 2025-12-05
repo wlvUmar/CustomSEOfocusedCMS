7bc586acb6db2c1e0f46337835ed3ada8f332e9f
wlvUmar <U.Tolibov@wlv.ac.uk>
2025-12-05 19:33:20 +0500

    'sdf'

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ require_once '../core/Database.php';
 require_once '../core/Router.php';
 require_once '../core/Controller.php';
 require_once '../core/helpers.php';
-session_start();
 
 $router = new Router();
 
