fc6a5d91110bde8d39bc9a7cd2f92c004fa53434
wlvUmar <U.Tolibov@wlv.ac.uk>
2025-12-05 20:43:25 +0500

    'sdf'

--- public/deploy.php
+++ public/deploy.php
@@ -1,6 +1,11 @@
 <?php
-$cmd = "cd /home/kuplyuta/appliances && git pull 2>&1";
+$cmd = <<<CMD
+cd /home/kuplyuta/appliances && \
+git stash push -m "auto-stash before deploy" 2>&1 && \
+git pull origin master 2>&1 && \
+git stash pop 2>&1
+CMD;
+
 echo "<pre>$cmd\n";
 $output = shell_exec($cmd);
 echo $output;
-
