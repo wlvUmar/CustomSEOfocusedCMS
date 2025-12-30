c64fa044a2611be35d9e8cb60b5e4cee69caec13
wlvUmar <U.Tolibov@wlv.ac.uk>
2025-12-30 20:26:38 +0500

    error login

--- public/index.php
+++ public/index.php
@@ -1,6 +1,34 @@
 <?php
 // path: ./public/index.php
 
+// Log all errors to a file
+ini_set('display_errors', 0); // don't show in browser
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/../logs/php_errors.log');
+error_reporting(E_ALL);
+
+// Catch standard PHP errors
+set_error_handler(function ($severity, $message, $file, $line) {
+    $log = "[Error][$severity] $message in $file on line $line\n";
+    file_put_contents(__DIR__ . '/../logs/php_errors.log', $log, FILE_APPEND);
+});
+
+// Catch uncaught exceptions
+set_exception_handler(function ($exception) {
+    $log = "[Exception] " . $exception->getMessage() . 
+           " in " . $exception->getFile() . 
+           " on line " . $exception->getLine() . "\n";
+    file_put_contents(__DIR__ . '/../logs/php_errors.log', $log, FILE_APPEND);
+});
+
+// Catch fatal errors
+register_shutdown_function(function () {
+    $error = error_get_last();
+    if ($error && in_array($error['type'], [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE])) {
+        $log = "[Fatal] {$error['message']} in {$error['file']} on line {$error['line']}\n";
+        file_put_contents(__DIR__ . '/../logs/php_errors.log', $log, FILE_APPEND);
+    }
+});
 require_once '../config/database.php';
 require_once '../config/config.php';
 require_once '../core/Database.php';
