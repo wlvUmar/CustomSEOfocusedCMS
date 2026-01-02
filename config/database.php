6c4585b45b465a7e03b83159302e0fd3d144f6b1
wlvUmar <U.Tolibov@wlv.ac.uk>
2026-01-02 14:31:17 +0500

    'prod'

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_NAME', getenv('DB_NAME') ?: 'appliances_db');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'kuplyuta_db');
+define('DB_USER', getenv('DB_USER') ?: 'kuplyuta_admin');
+define('DB_PASS', getenv('DB_PASS') ?: '=@iX?z~gukWg');
 define('DB_CHARSET', getenv('DB_CHARSET') ?: 'utf8mb4');
+	
