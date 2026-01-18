a1a49c777730a7919508fcd7cf01e5970e3c8388
wlvUmar <U.Tolibov@wlv.ac.uk>
2026-01-18 17:20:21 +0500

    Update security.php

--- config/security.php
+++ config/security.php
@@ -15,21 +15,24 @@ if (IS_PRODUCTION) {
 $csp = [
     "default-src 'self'",
     "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://www.googletagmanager.com",
+    "script-src-elem 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://www.googletagmanager.com",
+
     "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https:",
     "style-src-elem 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https:",
-    "script-src-elem 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://www.googletagmanager.com",
-    "img-src 'self' data: blob: https: https://www.google-analytics.com https://www.googletagmanager.com",
+
+    "img-src 'self' data: blob: https: https://www.google-analytics.com https://region1.google-analytics.com https://www.googletagmanager.com",
     "font-src 'self' data: https://cdnjs.cloudflare.com",
-    "connect-src 'self' https://unpkg.com https://cdnjs.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com",
+
+    "connect-src 'self' https://unpkg.com https://cdnjs.cloudflare.com https://www.googletagmanager.com https://www.google-analytics.com https://analytics.google.com https://region1.google-analytics.com",
+
     "frame-src 'self' https://www.googletagmanager.com",
     "frame-ancestors 'self'",
     "worker-src 'self' blob:",
     "child-src 'self' blob:"
 ];
 
-
-
 header('Content-Security-Policy: ' . implode('; ', $csp));
+
 
 
 // --------------------
